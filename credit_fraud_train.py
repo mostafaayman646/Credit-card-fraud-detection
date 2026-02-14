4f70640c450d24628e08708aec9dcfc11692b7cd
Mostafa <Mostafa.Elsayed03@eng-st.cu.edu.eg>
2026-02-14 08:51:26 +0200

    Added some techniques to solve imbalanced dataset issue

--- credit_fraud_train.py
+++ credit_fraud_train.py
@@ -9,17 +9,20 @@ import numpy as np
 
 #Constants:
 SEED = 17
+Under_sampling_factor = None
+Over_sampling_factor = 10
 
 def train(X_train,t_train,X_val,add_intercept):
     #Initialize models
     lr = LogisticRegression(solver='lbfgs',random_state=SEED,fit_intercept=add_intercept)
     rfc = RandomForestClassifier(n_estimators=50,max_depth=10)
-    nn = MLPClassifier(random_state=SEED,max_iter=300)
+    nn = MLPClassifier(random_state=SEED,max_iter=3000,hidden_layer_sizes=
+                       (10,10,10))
     
     #Voting
     vcf = VotingClassifier([
-        ('lr',lr),('rfc',rfc),('nn',nn)
-    ],voting='soft',weights=[1,2,1])
+        ('lr',lr),('rfc',rfc)
+    ],voting='soft',weights=[1,2])
     
     pip = Pipeline([
         ('Poly',PolynomialFeatures(degree=1,include_bias=add_intercept)),
@@ -37,10 +40,12 @@ def train(X_train,t_train,X_val,add_intercept):
 if __name__ == '__main__':
     np.random.seed(SEED)  # If you want to fix the results
     
-    X_train,t_train,X_val,t_val = load_data()
+    X_train,t_train,X_val,t_val = load_data(SEED,Over_sampling_factor=Over_sampling_factor,
+                                            Under_sampling_factor=Under_sampling_factor)
     
     y_train,y_val = train(X_train,t_train,X_val,add_intercept=True)
     
-    desc = "Using voting classifier between random forrest and nn and logistic regression and minmax scale on data only"
+    desc = "Using voting classifier between random forrest and logistic regression and minmax scale on data and also OverSampling using RandomOverSampling"
     
     evaluate(t_train,y_train,t_val,y_val,description=desc,save_results=True)
+    # evaluate(t_train,y_train,t_val,y_val)
--- credit_fraud_utils_data.py
+++ credit_fraud_utils_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler,NearMiss
+from imblearn.over_sampling import SMOTE,RandomOverSampler
 
-def load_data():
+def load_data(SEED,Over_sampling_factor = None,Under_sampling_factor=None):
     data_train = pd.read_csv('../data/split/train.csv')
     data_val = pd.read_csv('../data/split/val.csv')
     
@@ -9,4 +11,19 @@ def load_data():
     X_val = data_val.drop(columns=['Class']).to_numpy()
     y_val = data_val['Class'].to_numpy()
 
+    if Under_sampling_factor:
+        minority_size = (y_train == 1).sum()
+        # rus = RandomUnderSampler(random_state=SEED,sampling_strategy={0: Under_sampling_factor*minority_size})
+        nm = NearMiss(sampling_strategy={0: Under_sampling_factor*minority_size})
+        X_train,y_train = nm.fit_resample(X_train,y_train)
+        print("After UnderSampling")
+        print(f"Class 1:{(y_train == 1).sum()} and Class 0: {(y_train == 0).sum()}")
+    
+    elif Over_sampling_factor:
+        minority_size = (y_train == 1).sum()
+        # smt = SMOTE(random_state=SEED,sampling_strategy={1:Over_sampling_factor*minority_size},k_neighbors=5)
+        ros = RandomOverSampler(random_state=SEED,sampling_strategy={1:Over_sampling_factor*minority_size})
+        X_train,y_train = ros.fit_resample(X_train,y_train)
+        print("After OverSampling")
+        print(f"Class 1:{(y_train == 1).sum()} and Class 0: {(y_train == 0).sum()}")
     return X_train,y_train,X_val,y_val
